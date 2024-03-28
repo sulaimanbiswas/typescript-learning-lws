bed15b2a1108f9129f8a1a84e316fa7b163118bc
Sulaiman Biswas <sulaimanbiswasbd@gmail.com>
2024-03-28 16:39:28 +0600

    Type Basic

---
+++ output/script.js
@@ -0,0 +1,34 @@
+"use strict";
+const country = "Bangladesh";
+console.log(country);
+const sum = (a, b) => {
+    return a + b;
+};
+console.log(sum(2, 3));
+let playerName = "Tamim";
+let age = 32;
+console.log(playerName);
+age = 58;
+console.log(playerName);
+let isHungry;
+isHungry = true;
+isHungry = "Yes";
+// Array
+let fruits = ["apple", 34, "mango", true, null, undefined, {}];
+fruits.push("orange");
+fruits.push(23);
+fruits.push(() => { });
+console.log(fruits);
+// Object
+let person = {
+    name: "Tamim",
+    age: 32,
+    isHungry: true,
+    hobbies: ["cricket", "football"],
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh",
+    },
+};
+person.hobbies.push("coding");
+console.log(person);
