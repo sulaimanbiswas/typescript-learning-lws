1340362b01e13b7f2a7629ce6e28d296548ae89c
Md Suliman <Sulaimanbiswasbd@gmail.com>
2024-11-21 15:45:00 +0600

    Add examples of explicit, union, and dynamic types; implement function and alias types in script.ts

--- output/script.js
+++ output/script.js
@@ -29,6 +29,71 @@ let person = {
         city: "Dhaka",
         country: "Bangladesh",
     },
+    about: "I am a cricketer",
 };
 person.hobbies.push("coding");
 console.log(person);
+person.about = "I am a cricketer";
+// Explicit types
+let countryName;
+countryName = "Bangladesh";
+let playerAge;
+playerAge = 32;
+let isPlaying;
+isPlaying = true;
+let fruitsName;
+fruitsName = ["apple", "mango"];
+let personInfo;
+personInfo = {
+    name: "Tamim",
+    age: 32,
+    isHungry: true,
+};
+// union types
+let unionTypes;
+unionTypes = "apple";
+unionTypes = 34;
+let multipleTypes;
+multipleTypes = ["apple", 34, "mango"];
+// dynamic types (any)
+let dynamicTypes;
+dynamicTypes = "apple";
+dynamicTypes = 34;
+dynamicTypes = true;
+dynamicTypes = {};
+dynamicTypes = [];
+dynamicTypes = () => { };
+let dynamicTypeArray;
+dynamicTypeArray = ["apple"];
+let dynamicTypeObject;
+dynamicTypeObject = {
+    age: 45,
+    name: "25",
+};
+// function
+let myFunc;
+myFunc = () => {
+    console.log("Hello");
+};
+myFunc();
+const yourFunc = (a, b) => {
+    console.log(`Hello ${a} & ${b}`);
+};
+yourFunc("a", "b");
+// optional
+const optionalFunc = (a, b) => {
+    console.log(`Hello ${a} & ${b}`);
+};
+optionalFunc("hello");
+// void and undifind
+const numFunc = (a, b) => {
+    return a + b;
+};
+numFunc(5, 3);
+// Aliase Type
+const userDetails = (id, user) => {
+    console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+const sayHello = (user) => {
+    console.log(`hello ${user.age > 50 ? "Sir" : "Mr"} ${user.name}`);
+};
--- src/script.ts
+++ src/script.ts
@@ -155,3 +155,19 @@ const numFunc = (a: number, b: number): number => {
 };
 
 numFunc(5, 3);
+
+// Aliase Type
+
+type stringOrNumber = string | number;
+type userType = {
+  name: string;
+  age: number;
+};
+
+const userDetails = (id: stringOrNumber, user: userType) => {
+  console.log(`user id is ${id}, name is ${user.name} and age is ${user.age}`);
+};
+
+const sayHello = (user: userType) => {
+  console.log(`hello ${user.age > 50 ? "Sir" : "Mr"} ${user.name}`);
+};
