7cb1b8a141e2930a79edaabf2a13545e9d183784
Md Suliman <Sulaimanbiswasbd@gmail.com>
2024-11-21 18:53:29 +0600

    Add initial enum and APIResponse interface definitions in TypeScript

---
+++ output/enum/index.js
@@ -0,0 +1,20 @@
+"use strict";
+// enum RType {
+//   SUCCESS,
+//   FAILURE,
+//   UNAUTHTICATED,
+//   FORBIDDEN,
+// }
+// interface APIResponse<T> {
+//   status: number;
+//   type: RType;
+//   data: T;
+// }
+// const response2: APIResponse<object> = {
+//   status: 400,
+//   type: RType.FAILURE,
+//   data: {
+//     name: "Sulaiman",
+//     age: 25,
+//   },
+// };
---
+++ src/enum/index.ts
@@ -0,0 +1,21 @@
+// enum RType {
+//   SUCCESS,
+//   FAILURE,
+//   UNAUTHTICATED,
+//   FORBIDDEN,
+// }
+
+// interface APIResponse<T> {
+//   status: number;
+//   type: RType;
+//   data: T;
+// }
+
+// const response2: APIResponse<object> = {
+//   status: 400,
+//   type: RType.FAILURE,
+//   data: {
+//     name: "Sulaiman",
+//     age: 25,
+//   },
+// };
